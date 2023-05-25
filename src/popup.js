c9f1dff1747accda31678fe2171aae828468a326
Wouter Nolten <wouternolten@gmail.com>
2023-05-25 16:07:43 +0200

    Add functionality to create header

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.getElementById('slider');
+function getElement(elName) {
+  return document.getElementById(elName);
+}
+
+function addSliderControls() {
+  const slider = getElement('slider');
 
   slider.addEventListener('change', (element) => {
     if (element.target.value < 1) {
-      document.getElementById('slider-label').innerHTML = 'Disabled';
+      getElement('slider-label').innerHTML = 'Disabled';
       return;
     }
 
-    document.getElementById('slider-label').innerHTML = 'Enabled';
+    getElement('slider-label').innerHTML = 'Enabled';
   });
+}
+
+function addHeaderControls() {
+  const button = getElement('addHeader');
+
+  button.addEventListener('click', () => {
+    const headersDiv = document.getElementById('headers');
+
+    const headerNameField = document.createElement('input');
+    headerNameField.type = 'text';
+    headerNameField.placeholder = 'Header Name';
+
+    const headerValueField = document.createElement('input');
+    headerValueField.type = 'text';
+    headerValueField.placeholder = 'Header Value';
+
+    headersDiv.appendChild(headerNameField);
+    headersDiv.appendChild(headerValueField);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  addSliderControls();
+  addHeaderControls();
 });

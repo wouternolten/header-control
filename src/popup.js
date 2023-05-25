94f0cc833ed631ad7f1b48204eb29309999a2c2b
Wouter Nolten <wouternolten@gmail.com>
2023-05-25 15:51:13 +0200

    Use eslint to enforce javascript style

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var slider = document.getElementById('slider');
-    slider.addEventListener('change', function() {
-        console.log('Slider value: ' + this.value);
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.getElementById('slider');
+
+  slider.addEventListener('change', () => {
+    if (this.value < 1) {
+      document.getElementById('slider-label').innerHTML = 'Disabled';
+      return;
+    }
+
+    document.getElementById('slider-label').innerHTML = 'Enabled';
+  });
 });
